b29fc2cf0835758cb58d9995e249e5180ac2ca9e
Paul Stovell <paul@paulstovell.com>
2013-02-11 11:35:00 +1000

    Added unit tests

---
+++ source/SilverSardine.Tests/Web/Controllers/HomeControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using NUnit.Framework;
+using SilverSardine.Web.Controllers;
+
+namespace SilverSardine.Tests.Web.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        private HomeController controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            controller = new HomeController();
+        }
+
+        [Test]
+        public void Index_ShouldSayHello()
+        {
+            var result = controller.Index() as ViewResult;
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(controller.ViewBag.Message, Is.EqualTo("Hello world!"));
+        }
+    }
+}
